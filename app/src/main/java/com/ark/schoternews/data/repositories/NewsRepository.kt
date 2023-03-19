f2eee916bd911a9bf1b08c34c979f780eb809208
aditgocendra <gocendra123@gmail.com>
2023-03-19 11:22:29 +0700

    add get and add data article to sqlite

--- app/src/main/java/com/ark/schoternews/data/repositories/NewsRepository.kt
+++ app/src/main/java/com/ark/schoternews/data/repositories/NewsRepository.kt
@@ -1,16 +1,30 @@
 package com.ark.schoternews.data.repositories
 
-import com.ark.schoternews.core.utility.RetrofitInstance
+import com.ark.schoternews.data.datasource.local.AppDatabase
+import com.ark.schoternews.data.datasource.remote.NewsRemote
+import com.ark.schoternews.data.models.Article
 import com.ark.schoternews.data.models.ResponseNews
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import retrofit2.Response
 
-class NewsRepository {
+class NewsRepository(private val dbLocal : AppDatabase, private val newsRemote : NewsRemote) {
 
     suspend fun getNews(query : String) : Response<ResponseNews> {
         return withContext(Dispatchers.IO){
-            RetrofitInstance.api.getNews(query, "id")
+            newsRemote.getNews(query, "id")
+        }
+    }
+
+    suspend fun getArticle() : List<Article> {
+        return withContext(Dispatchers.IO){
+            dbLocal.articleDao().get()
+        }
+    }
+
+    suspend fun addArticle(article : Article) {
+        withContext(Dispatchers.IO){
+            dbLocal.articleDao().add(article)
         }
     }
 }
