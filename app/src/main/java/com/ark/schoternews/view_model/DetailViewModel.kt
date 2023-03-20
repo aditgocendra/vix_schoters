11d871ddea128fde27078ee6ef96fdd26812bb29
aditgocendra <gocendra123@gmail.com>
2023-03-20 11:07:05 +0700

    select article

--- app/src/main/java/com/ark/schoternews/view_model/DetailViewModel.kt
+++ app/src/main/java/com/ark/schoternews/view_model/DetailViewModel.kt
@@ -1,5 +1,6 @@
 package com.ark.schoternews.view_model
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.ark.schoternews.data.models.Article
@@ -7,10 +8,23 @@ import com.ark.schoternews.data.repositories.NewsRepository
 import kotlinx.coroutines.launch
 
 class DetailViewModel(private val newsRepository: NewsRepository) : ViewModel() {
+    val selectArticleData = MutableLiveData<Article>()
 
-    fun bookmarkArticle(article : Article){
+    fun selectArticle(title : String) {
         viewModelScope.launch {
-            newsRepository.addArticle(article)
+            val result = try {
+                newsRepository.selectArticle(title)
+            }catch (e : Exception){
+                return@launch
+            }
+
+            selectArticleData.value = result
+        }
+    }
+
+    fun bookmarkAdd(article: Article){
+        viewModelScope.launch {
+            newsRepository.addArticle(article = article)
         }
     }
 }
